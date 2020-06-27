e8344664c603b98bcf1c02e512cfb67763a11e78
Petros Kalogiannakis <github@petroskal.com>
2020-06-27 21:17:19 +0300

    Add default movies image

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,25 @@
+import defaultImage from '../src/assets/default.jpeg';
+
 export const buildImageUrl = (configuration, path, size) => {
+  let imageUrl = null;
+
   if (
     typeof configuration === 'undefined' ||
     Object.keys(configuration).length === 0
   ) {
-    return null;
+    return defaultImage;
   }
 
   if (typeof path === 'undefined' || path === null || path.length === 0) {
-    return null;
+    return defaultImage;
   }
 
   const baseUrl = configuration.secure_base_url;
   const posterSize = configuration.poster_sizes[size];
 
   if (typeof posterSize === 'undefined') {
-    return null;
+    return defaultImage;
   }
-
-  let imageUrl = null;
 
   if (path) {
     imageUrl = `${baseUrl}${posterSize}${path}`;
