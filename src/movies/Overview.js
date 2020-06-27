3c8804e4d8a5878ae69602c3cc8156a8be130d82
Petros Kalogiannakis <github@petroskal.com>
2020-06-27 20:58:50 +0300

    Move Trailers to new file

--- src/movies/Overview.js
+++ src/movies/Overview.js
@@ -1,48 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { TrailerModal } from '../components/TrailerModal';
-
-const Trailers = (props) => {
-  const [results, setResults] = useState([]);
-  const { id: id } = props;
-
-  useEffect(() => {
-    const fetchDetails = () => {
-      const apiKey = 'bc50218d91157b1ba4f142ef7baaa6a0';
-      const endpoint = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}`;
-
-      fetch(endpoint, {
-        method: 'GET',
-      })
-        .then((response) => {
-          if (response.status !== 200) throw new Error(response.status);
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setResults(data.results);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-
-    fetchDetails();
-  }, [id]);
-
-  const handleClick = () => {};
-
-  const trailers = results.slice(0, 1).map((result, index) => {
-    return <TrailerModal key={`movie-trailer-${index}`} id={result.key} />;
-  });
-
-  return <div className="movie__details__trailers">{trailers}</div>;
-};
-
-Trailers.propTypes = {
-  id: PropTypes.number.isRequired,
-};
+import { Trailers } from './Trailers';
 
 export const Overview = (props) => {
   const [data, setData] = useState([]);
