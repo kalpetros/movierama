7d3e7078f1471eecaae6ccf8d96f82f95b20adea
Petros Kalogiannakis <github@petroskal.com>
2020-06-28 01:12:54 +0300

    Fix utils test cases

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,42 +1,44 @@
 import { buildImageUrl } from '../src/utils';
 
-const configuration = {
-  secure_base_url: 'movierama.com/',
-  poster_sizes: [1, 2, 3, 4],
-};
-const path = '/test_path';
+describe('buildImageUrl', () => {
+  const configuration = {
+    secure_base_url: 'movierama.com/',
+    poster_sizes: [1, 2, 3, 4],
+  };
+  const path = '/test_path';
 
-test('Get image url', () => {
-  const imageUrl = buildImageUrl(configuration, path, 2);
-  expect(imageUrl).toBe('movierama.com/3/test_path');
-});
+  it('should return the complete url', () => {
+    const imageUrl = buildImageUrl(configuration, path, 2);
+    expect(imageUrl).toBe('movierama.com/3/test_path');
+  });
 
-test('Pass empty configuration', () => {
-  const imageUrl = buildImageUrl({}, path, 2);
-  expect(imageUrl).toBeNull();
-});
+  it('should return correctly given an empty configuration', () => {
+    const imageUrl = buildImageUrl({}, path, 2);
+    expect(imageUrl).toBeDefined();
+  });
 
-test('Pass undefined configuration', () => {
-  const imageUrl = buildImageUrl(undefined, path, 2);
-  expect(imageUrl).toBeNull();
-});
+  it('should return correctly given an undefined configuration', () => {
+    const imageUrl = buildImageUrl(undefined, path, 2);
+    expect(imageUrl).toBeDefined();
+  });
 
-test('Pass empty path', () => {
-  const imageUrl = buildImageUrl(configuration, '', 2);
-  expect(imageUrl).toBeNull();
-});
+  it('should return correctly given an empty path', () => {
+    const imageUrl = buildImageUrl(configuration, '', 2);
+    expect(imageUrl).toBeDefined();
+  });
 
-test('Pass undefined path', () => {
-  const imageUrl = buildImageUrl(configuration, undefined, 2);
-  expect(imageUrl).toBeNull();
-});
+  it('should return correctly given an undefined path', () => {
+    const imageUrl = buildImageUrl(configuration, undefined, 2);
+    expect(imageUrl).toBeDefined();
+  });
 
-test('Pass null path', () => {
-  const imageUrl = buildImageUrl(configuration, null, 2);
-  expect(imageUrl).toBeNull();
-});
+  it('should return correctly given a null path', () => {
+    const imageUrl = buildImageUrl(configuration, null, 2);
+    expect(imageUrl).toBeDefined();
+  });
 
-test('Pass incorrect size', () => {
-  const imageUrl = buildImageUrl(configuration, path, 5);
-  expect(imageUrl).toBeNull();
+  it('should return correctly given a size that does not exist', () => {
+    const imageUrl = buildImageUrl(configuration, path, 5);
+    expect(imageUrl).toBeDefined();
+  });
 });
