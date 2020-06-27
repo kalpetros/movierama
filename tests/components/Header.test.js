823d8bbe5b23211932a445948c8f75b37e6cf766
Petros Kalogiannakis <github@petroskal.com>
2020-06-27 23:35:52 +0300

    Add Header test cases

--- tests/components/Header.test.js
+++ tests/components/Header.test.js
@@ -1,11 +1,62 @@
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { configure, mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-import { SimilarMovies } from '../../src/movies/SimilarMovies';
+import { Header } from '../../src/components/Header';
+import { MoviesContext } from '../../src/store/MoviesContext';
 
-test('test', () => {
-  const wrapper = shallow(<SimilarMovies />);
+describe('<Header />', () => {
+  let wrapper;
+
+  it('should match the snapshot', () => {
+    const mockContext = {
+      movies: [],
+      query: 'Test',
+      setQuery: jest.fn(),
+    };
+
+    wrapper = mount(
+      <MoviesContext.Provider value={mockContext}>
+        <Header />
+      </MoviesContext.Provider>
+    );
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('should display movie title text', () => {
+    const mockContext = {
+      movies: [],
+      query: 'Test',
+      setQuery: jest.fn(),
+    };
+
+    wrapper = mount(
+      <MoviesContext.Provider value={mockContext}>
+        <Header />
+      </MoviesContext.Provider>
+    );
+
+    const title = wrapper.find('.header__title').find('h1').text();
+    expect(title).toBe('Movies "Test"');
+  });
+
+  it('should display in theaters text', () => {
+    const mockContext = {
+      movies: [],
+      query: '',
+      setQuery: jest.fn(),
+    };
+
+    wrapper = mount(
+      <MoviesContext.Provider value={mockContext}>
+        <Header />
+      </MoviesContext.Provider>
+    );
+
+    const title = wrapper.find('.header__title').find('h1').text();
+    expect(title).toBe('In Theaters');
+  });
 });
