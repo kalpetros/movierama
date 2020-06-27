890cc18d851e37004d00a3f05e422a5489680de3
Petros Kalogiannakis <github@petroskal.com>
2020-06-27 18:15:58 +0300

    Remove unused import from SimilarMovies.test

--- tests/movies/SimilarMovies.test.js
+++ tests/movies/SimilarMovies.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { configure, render, shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
