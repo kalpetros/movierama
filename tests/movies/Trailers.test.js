0812b7a2b75b166e24ad0338e30cdb0905905f6d
Petros Kalogiannakis <github@petroskal.com>
2020-06-28 01:25:13 +0300

    Add Trailers test case

---
+++ tests/movies/Trailers.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import { Trailers } from '../../src/movies/Trailers';
+
+describe('<Trailers />', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Trailers id={1} />);
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
